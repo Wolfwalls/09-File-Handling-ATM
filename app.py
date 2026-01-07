89ac11a4677cd4ce579196a172ac27c7da41fbaa
WolfWalls <djh09511@gmail.com>
2026-01-07 09:27:22 -0500

    initial fix

--- app.py
+++ app.py
@@ -5,11 +5,15 @@ if not os.path.exists("accounts.json"):
     exit()
 
 while True:
-    Acc_numb = input("Enter an account number: ").lower()
-    if Acc_numb == "q":
-        exit()
-    elif  Acc_numb != int:
-        print("That's not an integer.")
-    pin = int(input("Enter your 4-digit pin: "))
-    with open("accounts.json", "r")as file:
-        log = json.load(file)
+    try:
+        Acc_numb = input("Enter an account number: ").lower()
+        pin = int(input("Enter your 4-digit pin: "))
+        with open("accounts.json", "r")as file:
+            log = json.load(file)
+        if Acc_numb != log["accountNumber"]:
+            print("You're account number does not match.")
+    except:
+        if Acc_numb == "q":
+            exit()
+        elif  Acc_numb != int:
+            print("That's not an integer.")
